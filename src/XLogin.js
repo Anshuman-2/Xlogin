355a51cba2f2bb9c6ffb50f8eb424a189b27b937
Anshuman Sarkar <55441@hexaware.com>
2026-02-01 01:54:42 +0530

    Migrate to Create React App: clean slate

--- src/XLogin.js
+++ src/XLogin.js
@@ -20,39 +20,35 @@ function XLogin() {
     <div style={{ maxWidth: 300, margin: '40px auto', padding: 20, border: '1px solid #ccc', borderRadius: 8 }}>
       <h2 style={{ textAlign: 'center', marginBottom: 24 }}>Login Page</h2>
       <form onSubmit={handleSubmit}>
-        <div style={{ marginBottom: 16 }}>
-          <label style={{ display: 'block', marginBottom: 8 }}>
-            Username
-            <input
-              id="username"
-              name="username"
-              data-testid="username-input"
-              type="text"
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-              style={{ width: '100%', padding: 8, marginTop: 4, display: 'block' }}
-            />
-          </label>
-        </div>
-        <div style={{ marginBottom: 16 }}>
-          <label style={{ display: 'block', marginBottom: 8 }}>
-            Password
-            <input
-              id="password"
-              name="password"
-              data-testid="password-input"
-              type="password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              style={{ width: '100%', padding: 8, marginTop: 4, display: 'block' }}
-            />
-          </label>
-        </div>
+        <label>
+          Username
+          <input
+            id="username"
+            name="username"
+            data-testid="username-input"
+            type="text"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            style={{ width: '100%', padding: 8, marginTop: 4 }}
+          />
+        </label>
+        <label style={{ marginTop: 16 }}>
+          Password
+          <input
+            id="password"
+            name="password"
+            data-testid="password-input"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            style={{ width: '100%', padding: 8, marginTop: 4 }}
+          />
+        </label>
         <button
           type="submit"
           data-testid="submit-button"
           disabled={!isFormValid}
-          style={{ width: '100%', padding: 10 }}
+          style={{ width: '100%', padding: 10, marginTop: 16 }}
         >
           Submit
         </button>
